95256546c2773128d2c925b1bf9ec1c529078e84
Keshav <krg3@illinois.edu>
2022-08-17 13:55:00 -0500

    VIF, MSE, MSE revised.

--- models/ridge_regressor.py
+++ models/ridge_regressor.py
@@ -1,4 +1,4 @@
-# 8/8/22
+# 8/16/22
 
 import io
 import os
@@ -80,13 +80,10 @@ for item in file_to_parse.fetch():
     # Mapping quality scores
     align_qual = item.query_alignment_qualities
 
-    # Mapping reference positions - original position of each nucleotide in the BAM record (deletions would have
-    # "skipping" of numbers) --> current base and the base before (combinations of letters) --> assign a number -->
-    # encode categorically
-
+    # Mapping reference positions
     ref_pos = item.get_reference_positions()
 
-    # Mapping lack of paired reads (and binarize)
+    # Mapping lack of paired reads (to binarize)
     unmap_mate = int(item.mate_is_unmapped)
 
     # Append to master lists
@@ -99,28 +96,31 @@ for item in file_to_parse.fetch():
 print(f'100% complete')
 file_to_parse.close()
 
-# Turn list (of lists) into a dataframe
+# Turn list of lists into a dataframe
 
 subsample_sam = pd.DataFrame(qual_list)
+
+# Pre-processing
 
 column_names = list(np.arange(0, len(subsample_sam.columns), 1))
 column_names = [str(i) for i in column_names]
 subsample_sam = subsample_sam.set_axis(column_names, axis='columns', inplace=False)
 
-subsample_sam['unmap_mate'] = unmap_mate
+# Adding features
 
+subsample_sam['unmap_mate'] = unmap_mate
 subsample_sam['sum'] = subsample_sam.sum(axis=1)
 subsample_sam['average'] = subsample_sam['sum'] / len(subsample_sam.columns)
 subsample_sam = subsample_sam.drop(columns=['sum'])
 
-# Read group
+# Adding read group as a feature
 
 file_to_parse = pysam.AlignmentFile(bam_file, check_sq=False)
 
 output_header = file_to_parse.header
 read_groups = output_header['RG']
 
-# Each dictionary is a separate read group - physicality of reads on slide
+### Each dictionary is a separate read group - physicality of reads on slide
 
 group_categories = {}
 
@@ -135,68 +135,20 @@ for entry in file_to_parse:
     categories_by_record.append(current_category)
 
 read_group_data = pd.DataFrame(categories_by_record, columns=['read_group'])
-
 read_group_data = pd.get_dummies(data=read_group_data, prefix='read_group', columns=['read_group'], drop_first=False)
 
 subsample_sam['read_group'] = read_group_data['read_group_3'] # first column only - hardcoded because of dummy variable
 # trap
 
-# Split data into X and y to predict quality scores from other features
-
-### Dependent variable - start with a single position being predicted by the metrics above
+# Pre-processing - fill in missing data and sample 1% of reads
 
 subsample_sam = subsample_sam.fillna(0)
 subsample_sam = subsample_sam.sample(frac=0.01, axis=0, random_state=42)
-
-subsample_sam = subsample_sam.loc[:, '210':'read_group']
-
-y = subsample_sam['230']
-
-### Dropping Y columns - need to figure out what columns to use!
-
-X = subsample_sam.drop(['230'], axis=1)
-
-# X = subsample_sam['read_group'] # temporary test
-
-### May have to vary testing size
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-# Observe shape of data
-
-y_train = y_train.to_numpy()
-y_train = y_train.reshape(len(y_train), 1)
-
-y_test = y_test.to_numpy()
-y_test = y_test.reshape(len(y_test), 1)
-
-print('Shape of original dataset:      ', subsample_sam.shape)
-print('Shape of input - training set:  ', X_train.shape)
-print('Shape of output - training set: ', y_train.shape)
-print('Shape of input - testing set:   ', X_test.shape)
-print('Shape of output - testing set:  ', y_test.shape)
-
-# Cross-validation  to evaluate model
-cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=42)
-
-# Define and fit ridge regressor
-
-def normal_equation_ridge_regression(X, y, alpha):
-    intercept_ones = np.ones((len(X), 1))        # results in array( [ [1],..,[1] ] )
-    X_b = np.c_[intercept_ones, X]               # we now add the additional ones as a new column to our X
-    I = np.identity(X_b.shape[1])               # identity matrix with dimensions (n+1)
-    I[0][0] = 0                                 # adjusting the first value in I to be 0, to account for the intercept term
-    theta_optimal = np.linalg.inv(X_b.T.dot(X_b) + alpha * I).dot(X_b.T).dot(y) # the normal equation
-    return theta_optimal
-
-ridge_model = RidgeCV(alphas=np.arange(0, 1, 0.01), cv=cv, scoring='neg_mean_absolute_error')
-ridge_model = ridge_model.fit(X_train, y_train)
-theta_ridge_ne = np.array([ridge_model.intercept_, ridge_model.coef_[0]])
-
-print('Alpha parameter:', ridge_model.alpha_)
+# subsample_sam = subsample_sam.loc[:, '0':f'{len(subsample_sam.columns) - 4}']
+subsample_sam = subsample_sam.loc[:, '0':'20']
 
 def mse(y, y_predicted):
-    error = y-y_predicted
+    error = y - y_predicted
     loss = 1/(y.size) * np.dot(error.T, error)
     return loss
 
@@ -214,49 +166,93 @@ def getRidgeMSEFunction(alpha=0.0001):
 
     return ridgeMSE
 
-# Testing for overfitting
+def ridge_regressor(data_frame):
 
-print('MSE for training data:', mse(y_train, ridge_model.predict(X_train)))
-print('MSE for testing data:', mse(y_test, ridge_model.predict(X_test)))
+    y_preds_list = []
+    error_list = []
+
+    for i in data_frame.columns:
+
+        y = data_frame[i]
+        X = data_frame.drop([i], axis=1)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+        # Cross-validation for model
+
+        cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=42)
+
+        # Define and fit ridge regressor
+
+        ridge_model = RidgeCV(alphas=np.arange(0, 1, 0.01), cv=cv, scoring='neg_mean_absolute_error')
+        ridge_model = ridge_model.fit(X_train, y_train)
+        theta_ridge = ridge_model.intercept_, ridge_model.coef_[0]
+        alpha_ridge = ridge_model.alpha_
+
+        # Predicted columns
+
+        y_pred = list(ridge_model.predict(X_train))
+        y_preds_list.append(y_pred)
+
+        mse_score = getRidgeMSEFunction(y_test, ridge_model.predict(X_test))
+        error_list.append(mse_score)
+
+    return y_preds_list, error_list
+
+y_preds_test, mse_test = ridge_regressor(subsample_sam)
+
+print(np.mean(mse_test))
+
+y_preds_test_df = pd.DataFrame(y_preds_test)
+y_preds_test_df = y_preds_test_df.transpose()
+
+sns.heatmap(y_preds_test_df, vmin=0, vmax=40, cmap='viridis')
+plt.show()
 
 # Testing for multicollinearity in X (for ridge regression)
 
 VIF_results = [variance_inflation_factor(X.values, i) for i in range(len(X.columns))]
 print('VIF for input data:', VIF_results)
 
-# Plots
-
-sns.set_theme(style='ticks')
-heatmap_colormap = 'viridis'
-
-qual_correlations = subsample_sam.corr()
-plt.title('Correlation Matrix - Evidence for Multicollinearity', fontweight='bold')
-sns.heatmap(qual_correlations, cmap=heatmap_colormap, center=0, square=True)
-plt.show()
-
-x_figs, (ax1, ax2) = plt.subplots(1, 2)
-
-x1 = sns.heatmap(X_train, ax=ax1, cmap=heatmap_colormap, cbar=True, cbar_kws=dict(use_gridspec=False, location='left'))
-x2 = sns.heatmap(X_test, ax=ax2, cmap=heatmap_colormap, cbar=False)
-x1.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='positions along read (X train features)', ylabel='reads')
-x2.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='positions along read (X test features)')
-
-plt.show()
-
-y_figs, (ax3, ax4, ax5) = plt.subplots(1, 3)
-
-y1 = sns.heatmap(y_train, ax=ax3, cmap=heatmap_colormap, cbar=True, cbar_kws=dict(use_gridspec=False, location='left'))
-y2 = sns.heatmap(y_test, ax=ax4, cmap=heatmap_colormap, cbar=False)
-yp = sns.heatmap(data=ridge_model.predict(X_test), ax=ax5, cmap=heatmap_colormap, cbar=False) # y_pred
-y1.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y train quality scores)', ylabel='reads')
-y2.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y test quality scores)')
-yp.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y pred quality scores)')
-
-plt.show()
-
-residuals = ridge_model.predict(X_test) - y_test
-residuals = pd.DataFrame(residuals)
-residuals = sns.heatmap(residuals.sample(frac=0.25), annot=True, cmap=heatmap_colormap) # residual matrix
-residuals.set()
-
-plt.show()
+# # Plot themes
+#
+# sns.set_theme(style='ticks')
+# heatmap_colormap = 'viridis'
+#
+# # Correlation matrix
+#
+# qual_correlations = subsample_sam.corr()
+# plt.title('Correlation Matrix - Evidence for Multicollinearity', fontweight='bold')
+# sns.heatmap(qual_correlations, cmap=heatmap_colormap, center=0, square=True)
+# plt.show()
+#
+# # X and y features
+#
+# x_figs, (ax1, ax2) = plt.subplots(1, 2)
+#
+# x1 = sns.heatmap(X_train, ax=ax1, cmap=heatmap_colormap, cbar=True, cbar_kws=dict(use_gridspec=False, location='left'))
+# x2 = sns.heatmap(X_test, ax=ax2, cmap=heatmap_colormap, cbar=False)
+# x1.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='positions along read (X train features)', ylabel='reads')
+# x2.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='positions along read (X test features)')
+#
+# plt.show()
+#
+# y_figs, (ax3, ax4, ax5) = plt.subplots(1, 3)
+#
+# y1 = sns.heatmap(y_train, ax=ax3, cmap=heatmap_colormap, cbar=True, cbar_kws=dict(use_gridspec=False, location='left'))
+# y2 = sns.heatmap(y_test, ax=ax4, cmap=heatmap_colormap, cbar=False)
+# yp = sns.heatmap(data=ridge_model.predict(X_test), ax=ax5, cmap=heatmap_colormap, cbar=False) # y_pred
+# y1.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y train quality scores)', ylabel='reads')
+# y2.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y test quality scores)')
+# yp.set(xticks=[], yticks=[], xticklabels=[], yticklabels=[], xlabel='position along read (y pred quality scores)')
+#
+# plt.show()
+#
+# # Residuals and MSE
+#
+# residuals = ridge_model.predict(X_test) - y_test
+# residuals = pd.DataFrame(residuals)
+# residuals = sns.heatmap(residuals.sample(frac=0.25), annot=True, cmap=heatmap_colormap) # residual matrix
+# residuals.set()
+#
+# plt.show()
