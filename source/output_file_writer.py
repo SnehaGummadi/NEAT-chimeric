0eb81102df3bef55e216c9e4574698c9565cf1b2
Sneha Gummadi <sag200006@ganymede.utdallas.edu>
2024-05-22 19:12:08 -0500

    unable to get output csv file to work. will try another method

--- source/output_file_writer.py
+++ source/output_file_writer.py
@@ -107,7 +107,7 @@ class OutputFileWriter:
             chim_fq2 = pathlib.Path(out_prefix + 'chim_read2.fq.gz')
         bam = pathlib.Path(out_prefix + '_golden.bam')
         vcf = pathlib.Path(out_prefix + '_golden.vcf.gz')
-        chim_csv = pathlib.Path(out_prefix + '_chim.csv')
+        
 
         # TODO Make a fasta-specific method
         self.no_fastq = no_fastq
@@ -178,11 +178,7 @@ class OutputFileWriter:
                 self.bam_file.write(n[0] + '\0')
                 self.bam_file.write(pack('<i', n[3]))
 
-        # CSV OUTPUT
-        with open(chim_csv, 'a', newline='') as self.chim_csv_file:
-            fieldnames= ['read_name','read1','qual1','read2','qual2']
-            csvwriter = csv.DictWriter(self.chim_csv_file, fieldnames=fieldnames)
-            csvwriter.writeheader()
+        
 
 
         # buffers for more efficient writing
@@ -192,16 +188,7 @@ class OutputFileWriter:
         self.chim_fq2_buffer = []
         self.bam_buffer = []
 
-    def write_chim_csv(self, read_name, read1, qual1, read2, qual2):
-        self.chim_csv_file.writerow(
-            {
-                'read_name':read_name,
-                'read1':read1,
-                'qual1':qual1,
-                'read2':read2,
-                'qual2':qual2
-            }
-        )
+    
 
     # TODO add write_fasta_record
 
@@ -368,5 +355,4 @@ class OutputFileWriter:
             self.vcf_file.close()
         if self.bam_file is not None:
             self.bam_file.close()
-        if self.chim_csv is not None:
-            self.chim_csv.close()
+ 
